0d23f34ca4e63236dbfc011fceb420c329758f9b
sreevanivula <37394366+sreevanivula@users.noreply.github.com>
2018-05-11 15:02:46 +0530

    java project

---
+++ java/lingoport/com/lingoport/HelloWorld.java
@@ -0,0 +1,35 @@
+package com.lingoport;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String greeting = "Hello";
+		greeting += " World";
+		String goodbye = " Goodbye World";
+		String Seeyou = "See you tomorrow
+		String additional = "Additional embedded string";
+    
+		greeting.equalsIgnoreCase(goodbye);
+
+		DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT);
+		String dateString = dateFormatter.format(new Date());
+		
+		CustomDateObject customDateObject = new customDateObject();
+		customDateObject.MyExampleMethod()
+
+		Double inputNum = new Double("2.57");
+
+		System.out.println(greeting);
+		System.out.println(inputNum);
+		System.out.println(dateString);
+
+		Logger logger = Logger.getLogger("Logger");
+		String logMessage = "Logging message - Don't translate me.";
+		logger.log(Level.INFO, logMessage);
+	}
+}
